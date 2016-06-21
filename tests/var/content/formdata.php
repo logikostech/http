fb5cf717b77df9488ba3414c7b3576c54500c280
tempcke <t.bitbucket@logikostech.net>
2016-06-21 16:29:03 -0400

    test data

---
+++ tests/var/content/formdata.php
@@ -0,0 +1,23 @@
+<?php
+return (object) [
+  'post' => [
+    'firstName' => 'John',
+    'lastName' => 'Smith',
+    'age' => '25',
+    'street' => '134 2nd Street',
+    'city' => 'New York',
+    'state' => 'NY',
+    'zipcode' => '10021',
+    'type' => 'cell',
+    'number' => '646 555-4567',
+  ],
+  'files' => [
+    'singlefile' => [
+      'name' => 'accept.gif',
+      'type' => 'image/gif',
+      'tmp_name' => '/tmp/contentparseUhk1HQ',
+      'error' => 0,
+      'size' => 265,
+    ],
+  ]
+];
